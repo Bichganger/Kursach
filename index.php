d833a10cf72f82b9c4da02c88c1e3db44551752b
Bichganger <gad.2018.geniy@gmail.com>
2025-02-15 15:25:07 +0200

    ok

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<!-- <header>
+  <header class="bg-image" style="background-image: url('car-background.jpg');">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+
+      </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <div class="d-flex align-items-center w-100">
+           <a class="navbar-brand" href="#">
+            <img src="" alt="Логотип" height="30">
+          </a> 
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav" id="navbarNavLeft" style="order: 1;">
+              <li class="nav-item">
+                <a class="nav-link" href="#new">Новые</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#used">С пробегом</a>
+              </li>
+            </ul>
+
+            <div class="mx-auto" style="order: 2;"></div>  Отступ для логотипа 
+
+            <ul class="navbar-nav ms-auto" id="navbarNavRight" style="order: 3;">
+              <li class="nav-item">
+                <a class="nav-link" href="#services">Сервис</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contacts">Контакты</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header> -->
+
+        
+   
+
+    <main>
+    <header>
+  <header class="bg-image" style="background-image: url('car-background.jpg');">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+
+      </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <div class="d-flex align-items-center w-100">
+          <!-- <a class="navbar-brand" href="#">s
+            <img src="" alt="Логотип" height="30">
+          </a> -->
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav" id="navbarNavLeft" style="order: 1;">
+              <li class="nav-item">
+                <a class="nav-link" href="#new">Новые</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#used">С пробегом</a>
+              </li>
+            </ul>
+
+            <div class="mx-auto" style="order: 2;"></div> <!-- Отступ для логотипа -->
+
+            <ul class="navbar-nav ms-auto" id="navbarNavRight" style="order: 3;">
+              <li class="nav-item">
+                <a class="nav-link" href="#services">Сервис</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contacts">Контакты</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="img/DeWatermark.ai_1739258916641.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="img/DeWatermark.ai_1739258977275.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/DeWatermark.ai_1739258980187.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Предыдущий</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Следующий</span>
+  </button>
+</div>
+
+    <!-- <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/DeWatermark.ai_1739258916641.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="DeWatermark.ai_1739258977275.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="DeWatermark.ai_1739258980187.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div> -->
+
+
+    </main>
+
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
